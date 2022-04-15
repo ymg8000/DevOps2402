ed101a640150d2a6e066cba4b6d8b7a5af30938b
ymg8000 <ymg8000@gmail.com>
2022-04-15 18:30:45 +0300

    test commit

---
+++ 20.py
@@ -0,0 +1,4 @@
+from selenium import webdriver
+
+my_driver = webdriver.Chrome()
+my_driver.get("file:///Users/yigal/PycharmProjects/pythonProject2/index.html")
---
+++ main.py
@@ -0,0 +1,26 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    a = "Yigal"
+    print(a)
+    first = 7
+    second = 44.3
+    print_hi('PyCharm')
+    print(first+second)
+    print(first*second)
+    print(second/first)
+
+    a = 8
+    b = '123'
+    print(a)
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
